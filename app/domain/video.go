e541ae06c66d8b3c04bdaf48b0c9be5f2f707678
Thiago Silva <itm_tsilva@uolinc.com>
2022-11-03 20:25:35 +0000

    feat: inicializacao da struct
    adicionado func init para informar que a validacao dos campos required devem ser validas por padrao. Adiciona a lib govalidator para validacao da struct

--- app/domain/video.go
+++ app/domain/video.go
@@ -1,10 +1,33 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+)
 
 type Video struct {
-	ID         string // este e nosso id, interno do sistema
-	ResourceID string //este id é do sistema que enviara as informação
-	FilePath   string
-	CreatedAt  time.Time
+	ID         string    `valid:"uuid"`    // este e nosso id, interno do sistema
+	ResourceID string    `valid:"notnull"` //este id é do sistema que enviara as informação
+	FilePath   string    `valid:"notnull"`
+	CreatedAt  time.Time `valid:"-"`
+}
+
+func init() {
+	//roda primeiro
+	govalidator.SetFieldsRequiredByDefault(true) ///se por padrao os campos da struct for requerida ele vai validar
+}
+
+func NewVideo() *Video {
+	return &Video{}
+}
+
+func (v *Video) Validate() error {
+	_, err := govalidator.ValidateStruct(v)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
