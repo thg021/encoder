411ddb4fedf8e67b3dbdd8bb80aee219b35c3265
Thiago Silva <itm_tsilva@uolinc.com>
2022-11-04 11:36:52 +0000

    feat: configuracao do gorm e entidades
    adicionado types para utilizacao do banco de dados

--- app/domain/video.go
+++ app/domain/video.go
@@ -7,10 +7,11 @@ import (
 )
 
 type Video struct {
-	ID         string    `valid:"uuid"`    // este e nosso id, interno do sistema
-	ResourceID string    `valid:"notnull"` //este id é do sistema que enviara as informação
-	FilePath   string    `valid:"notnull"`
-	CreatedAt  time.Time `valid:"-"`
+	ID         string    `json:"encoded_video_folder" valid:"uuid" gorm:"type:uuid;primary_key"` // este e nosso id, interno do sistema /// O nome do video convertido ser o nome da pasta na nuvem
+	ResourceID string    `json:"resource_id" valid:"notnull" gorm:"type:varchar(255)"`           //este id é do sistema que enviara as informação
+	FilePath   string    `json:"file_path" valid:"notnull" gorm:"type:varchar(255)"`
+	CreatedAt  time.Time `json:"-" valid:"-"`
+	Jobs       []*Job    `json:"-" valid:"-" gorm:"ForeignKey:VideoID"`
 }
 
 func init() {
